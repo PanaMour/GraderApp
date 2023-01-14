3f92ac82565d1423d8c32b37eec7068b16677654
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-01-14 19:37:15 +0200

    Updated Layouts, added Secretary Controller

---
+++ Controllers/SecretariesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GraderApp.Models;
+
+namespace GraderApp.Controllers
+{
+    public class SecretariesController : Controller
+    {
+        private readonly GraderDBContext _context;
+
+        public SecretariesController(GraderDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Secretaries
+        public async Task<IActionResult> Index()
+        {
+            var graderDBContext = _context.Secretaries.Include(s => s.UsersUsernameNavigation);
+            return View(await graderDBContext.ToListAsync());
+        }
+
+        // GET: Secretaries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries
+                .Include(s => s.UsersUsernameNavigation)
+                .FirstOrDefaultAsync(m => m.Phonenumber == id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Create
+        public IActionResult Create()
+        {
+            ViewData["UsersUsername"] = new SelectList(_context.Users, "Username", "Username");
+            return View();
+        }
+
+        // POST: Secretaries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Phonenumber,Name,Surname,Department,UsersUsername")] Secretary secretary)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(secretary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsersUsername"] = new SelectList(_context.Users, "Username", "Username", secretary.UsersUsername);
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries.FindAsync(id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsersUsername"] = new SelectList(_context.Users, "Username", "Username", secretary.UsersUsername);
+            return View(secretary);
+        }
+
+        // POST: Secretaries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Phonenumber,Name,Surname,Department,UsersUsername")] Secretary secretary)
+        {
+            if (id != secretary.Phonenumber)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(secretary);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SecretaryExists(secretary.Phonenumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsersUsername"] = new SelectList(_context.Users, "Username", "Username", secretary.UsersUsername);
+            return View(secretary);
+        }
+
+        // GET: Secretaries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Secretaries == null)
+            {
+                return NotFound();
+            }
+
+            var secretary = await _context.Secretaries
+                .Include(s => s.UsersUsernameNavigation)
+                .FirstOrDefaultAsync(m => m.Phonenumber == id);
+            if (secretary == null)
+            {
+                return NotFound();
+            }
+
+            return View(secretary);
+        }
+
+        // POST: Secretaries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Secretaries == null)
+            {
+                return Problem("Entity set 'GraderDBContext.Secretaries'  is null.");
+            }
+            var secretary = await _context.Secretaries.FindAsync(id);
+            if (secretary != null)
+            {
+                _context.Secretaries.Remove(secretary);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SecretaryExists(int id)
+        {
+          return _context.Secretaries.Any(e => e.Phonenumber == id);
+        }
+    }
+}
